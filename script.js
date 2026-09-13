8218ed34753c82f2266ac2e52700844d381e7dd6
Brahim <61661606+BrahimELMokhtari@users.noreply.github.com>
2026-09-13 20:56:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const links=document.querySelectorAll('a[href^="#"]');links.forEach(a=>a.addEventListener('click',e=>{const t=document.querySelector(a.getAttribute('href'));if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth'})}}));
+const researchScroll = document.querySelector('.research-scroll');
+
+researchScroll.addEventListener('wheel', (event) => {
+  if (event.deltaY !== 0) {
+    event.preventDefault();
+    researchScroll.scrollLeft += event.deltaY;
+  }
+}, { passive: false });
